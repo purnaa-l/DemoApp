fc9e8c5ca0eaab787632980493f2ebce41454064
Purnaa <147434929+purnaa-l@users.noreply.github.com>
2024-11-17 21:04:49 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I am rich'),
+        ),
+        body: Center(
+          child: Image.asset('images/diamond.png'), // Simplified version
+        ),
+      ),
+    ),
+  );
+}
